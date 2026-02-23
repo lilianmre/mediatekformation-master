d49264b65d3cb16c11c4f9f2ec1aada5b6319fbe
Lilian <14lilianmarie14@gmail.com>
2026-02-23 11:26:49 +0100

    Tâche 4 : ajouter l'accès avec authentification - Mise en place d'une authentification pour sécuriser le back office : seul un profil administrateur y accéder. - Ajout d'un lien de déconnexion sur toutes les pages du back office.

---
+++ src/Controller/Admin/SecurityController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route('/admin/login', name: 'admin_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin_formations');
+        }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('admin/security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
+    }
+
+    #[Route('/admin/logout', name: 'admin_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('Cette méthode ne peut pas être vide.');
+    }
+}
